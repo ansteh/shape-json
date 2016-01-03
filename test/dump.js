2474c7164e8d02e52f96f7dc63ee021c485bd077
ansteh <stehle.andre@googlemail.com>
2016-01-03 23:52:56 +0100

    update tests

---
+++ test/dump.js
@@ -0,0 +1,55 @@
+/*let result = [{
+  id: 1,
+  last_name: 'Stehle',
+  first_name: 'Andre',
+  projects: [{
+    id: 1,
+    name: 'project 1',
+    urls: [{
+      id: 1,
+      name: 'project1_1.com'
+    },{
+      id: 2,
+      name: 'project1_2.com'
+    }]
+  },{
+    id: 3,
+    name: 'project 3',
+    urls: [{
+      id: 3,
+      name: 'project3_1.com'
+    },{
+      id: 4,
+      name: 'project3_2.com'
+    }]
+  },{
+    id: 4,
+    name: 'project 4',
+    urls: [{
+      id: 5,
+      name: 'project4_1.com'
+    }]
+  }]
+},{
+  id: 2,
+  last_name: 'Dalton',
+  first_name: 'John',
+  projects: [{
+    id: 6,
+    name: 'project 6',
+    urls: [{
+      id: 6,
+      name: 'project6_1.com'
+    }]
+  },{
+    id: 7,
+    name: 'project 7',
+    urls: [{
+      id: 7,
+      name: 'project7_1.com'
+    },{
+      id: 8,
+      name: 'project7_2.com'
+    }]
+  }]
+}];*/
--- test/spec/parse.js
+++ test/spec/parse.js
@@ -1,24 +1,49 @@
 'use strict';
 var shape = require('../../index.js');
 
-let inputs = [
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 1, projectName: 'project 1', url: 'project1.com'},
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 3, projectName: 'project 3', url: 'project3.com'},
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 4, projectName: 'project 4', url: 'project4.com'},
-  {pid: 2, lastName: 'Dalton', firstName: 'John',  projectID: 6, projectName: 'project 6', url: 'project6.com'},
-  {pid: 2, lastName: 'Dalton', firstName: 'John',  projectID: 7, projectName: 'project 7', url: 'project7.com'}
-];
+let input = require('../resources.js').projects;
+let _ = require('lodash');
 
-let inputsWithUrls = [
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 1, projectName: 'project 1', url: 'project1_1.com', urlID: 1},
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 1, projectName: 'project 1', url: 'project1_2.com', urlID: 2},
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 3, projectName: 'project 3', url: 'project3_1.com', urlID: 3},
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 3, projectName: 'project 3', url: 'project3_2.com', urlID: 4},
-  {pid: 1, lastName: 'Stehle', firstName: 'Andre', projectID: 4, projectName: 'project 4', url: 'project4_1.com', urlID: 5},
-  {pid: 2, lastName: 'Dalton', firstName: 'John',  projectID: 6, projectName: 'project 6', url: 'project6_1.com', urlID: 6},
-  {pid: 2, lastName: 'Dalton', firstName: 'John',  projectID: 7, projectName: 'project 7', url: 'project7_1.com', urlID: 7},
-  {pid: 2, lastName: 'Dalton', firstName: 'John',  projectID: 7, projectName: 'project 7', url: 'project7_2.com', urlID: 8}
-];
+let groupedUrls = _.map(_.groupBy(input, 'pid'), function(group){
+  let person = group[0];
+  return {
+    "id": person.pid,
+    "last_name": person.lastName,
+    "first_name": person.firstName,
+    "projects": _.map(_.groupBy(group, 'projectID'), function(projects){
+      let project = projects[0];
+      return {
+        "id": project.projectID,
+        "name": project.projectName,
+        "urls": _.map(projects, function(url){
+          return {
+            "id" : url.urlID,
+            "name": url.url
+          };
+        })
+      };
+    })
+  }
+});
+//console.log(groupedUrls[0]['projects'][0]);
+
+let groupedProjects = _.map(_.groupBy(input, 'pid'), function(group){
+  let person = group[0];
+  return {
+    "id": person.pid,
+    "last_name": person.lastName,
+    "first_name": person.firstName,
+    "projects": _.uniq(_.map(group, function(project){
+      return {
+        "id": project.projectID,
+        "name": project.projectName,
+        "url": project.url
+      };
+    }), 'id')
+  }
+});
+
+console.log(groupedProjects);
 
 describe("scheme form", function() {
   it("simple parse equals mirror", function() {
@@ -28,32 +53,18 @@ describe("scheme form", function() {
       "url": "url"
     };
 
-    let result = [{
-      id: 1,
-      name: 'project 1',
-      url: 'project1.com'
-    },{
-      id: 3,
-      name: 'project 3',
-      url: 'project3.com'
-    },{
-      id: 4,
-      name: 'project 4',
-      url: 'project4.com'
-    },{
-      id: 6,
-      name: 'project 6',
-      url: 'project6.com'
-    },{
-      id: 7,
-      name: 'project 7',
-      url: 'project7.com'
-    }];
+    let result = _.map(input, function(project){
+      return {
+        "id": project.projectID,
+        "name": project.projectName,
+        "url": project.url
+      };
+    });
 
-    expect(shape.parse(inputs, scheme)).toEqual(result);
+    expect(shape.parse(input, scheme)).toEqual(result);
   });
 
-  it("parse group persons", function() {
+  it("parse projects with a single depth", function() {
     let scheme = {
       "$group[persons](pid)": {
         "id": "pid",
@@ -66,39 +77,8 @@ describe("scheme form", function() {
         }
       }
     };
-
-    let result = [{
-      id: 1,
-      last_name: 'Stehle',
-      first_name: 'Andre',
-      projects: [{
-        id: 1,
-        name: 'project 1',
-        url: 'project1.com'
-      },{
-        id: 3,
-        name: 'project 3',
-        url: 'project3.com'
-      },{
-        id: 4,
-        name: 'project 4',
-        url: 'project4.com'
-      }]
-    },{
-      id: 2,
-      last_name: 'Dalton',
-      first_name: 'John',
-      projects: [{
-        id: 6,
-        name: 'project 6',
-        url: 'project6.com'
-      },{
-        id: 7,
-        name: 'project 7',
-        url: 'project7.com'
-      }]
-    }];
-    expect(shape.parse(inputs, scheme).persons).toEqual(result);
+    console.log(shape.parse(input, scheme).persons);
+    expect(shape.parse(input, scheme).persons).toEqual(groupedProjects);
   });
 
   it("parse group persons and urls", function() {
@@ -117,63 +97,7 @@ describe("scheme form", function() {
         }
       }
     };
-
-    let result = [{
-      id: 1,
-      last_name: 'Stehle',
-      first_name: 'Andre',
-      projects: [{
-        id: 1,
-        name: 'project 1',
-        urls: [{
-          id: 1,
-          name: 'project1_1.com'
-        },{
-          id: 2,
-          name: 'project1_2.com'
-        }]
-      },{
-        id: 3,
-        name: 'project 3',
-        urls: [{
-          id: 3,
-          name: 'project3_1.com'
-        },{
-          id: 4,
-          name: 'project3_2.com'
-        }]
-      },{
-        id: 4,
-        name: 'project 4',
-        urls: [{
-          id: 5,
-          name: 'project4_1.com'
-        }]
-      }]
-    },{
-      id: 2,
-      last_name: 'Dalton',
-      first_name: 'John',
-      projects: [{
-        id: 6,
-        name: 'project 6',
-        urls: [{
-          id: 6,
-          name: 'project6_1.com'
-        }]
-      },{
-        id: 7,
-        name: 'project 7',
-        urls: [{
-          id: 7,
-          name: 'project7_1.com'
-        },{
-          id: 8,
-          name: 'project7_2.com'
-        }]
-      }]
-    }];
-    expect(shape.parse(inputsWithUrls, scheme).persons).toEqual(result);
+    expect(shape.parse(input, scheme).persons).toEqual(groupedUrls);
   });
 
   it("parse group persons and urls avoiding embedded functions in scheme", function() {
@@ -196,121 +120,10 @@ describe("scheme form", function() {
       }
     };
 
-    let result = [{
-      id: 1,
-      last_name: 'Stehle',
-      first_name: 'Andre',
-      projects: [{
-        id: 1,
-        name: 'project 1',
-        urls: [{
-          id: 1,
-          name: 'project1_1.com'
-        },{
-          id: 2,
-          name: 'project1_2.com'
-        }]
-      },{
-        id: 3,
-        name: 'project 3',
-        urls: [{
-          id: 3,
-          name: 'project3_1.com'
-        },{
-          id: 4,
-          name: 'project3_2.com'
-        }]
-      },{
-        id: 4,
-        name: 'project 4',
-        urls: [{
-          id: 5,
-          name: 'project4_1.com'
-        }]
-      }]
-    },{
-      id: 2,
-      last_name: 'Dalton',
-      first_name: 'John',
-      projects: [{
-        id: 6,
-        name: 'project 6',
-        urls: [{
-          id: 6,
-          name: 'project6_1.com'
-        }]
-      },{
-        id: 7,
-        name: 'project 7',
-        urls: [{
-          id: 7,
-          name: 'project7_1.com'
-        },{
-          id: 8,
-          name: 'project7_2.com'
-        }]
-      }]
-    }];
-    expect(shape.parse(inputsWithUrls, scheme).persons).toEqual(result);
+    expect(shape.parse(input, scheme).persons).toEqual(groupedUrls);
   });
 
   it("parse group persons and urls as a scheme", function() {
-    let result = [{
-      id: 1,
-      last_name: 'Stehle',
-      first_name: 'Andre',
-      projects: [{
-        id: 1,
-        name: 'project 1',
-        urls: [{
-          id: 1,
-          name: 'project1_1.com'
-        },{
-          id: 2,
-          name: 'project1_2.com'
-        }]
-      },{
-        id: 3,
-        name: 'project 3',
-        urls: [{
-          id: 3,
-          name: 'project3_1.com'
-        },{
-          id: 4,
-          name: 'project3_2.com'
-        }]
-      },{
-        id: 4,
-        name: 'project 4',
-        urls: [{
-          id: 5,
-          name: 'project4_1.com'
-        }]
-      }]
-    },{
-      id: 2,
-      last_name: 'Dalton',
-      first_name: 'John',
-      projects: [{
-        id: 6,
-        name: 'project 6',
-        urls: [{
-          id: 6,
-          name: 'project6_1.com'
-        }]
-      },{
-        id: 7,
-        name: 'project 7',
-        urls: [{
-          id: 7,
-          name: 'project7_1.com'
-        },{
-          id: 8,
-          name: 'project7_2.com'
-        }]
-      }]
-    }];
-
     let scheme = shape
       .scheme()
       .parse({
@@ -329,6 +142,6 @@ describe("scheme form", function() {
         }
       });
 
-    expect(scheme.form(inputsWithUrls).persons).toEqual(result);
+    expect(scheme.form(input).persons).toEqual(groupedUrls);
   });
 });
